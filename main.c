2d4877f4ed8bd6f69ef36c5d0286c3d8a2ae7bac
SaptaRathiRoy <84488869+SaptaRathiRoy@users.noreply.github.com>
2023-07-15 14:37:31 +0530

    Array

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void array(int arr[], int size){
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element for index %d: ",i);
+        scanf("%d",&arr[i]);
+    }
+}
+
+void printArray(int arr[], int size){
+    printf("Elements in the array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int x;
+    printf("Enter number of elements: ");
+    scanf("%d",&x);
+    int arr[x];
+    array(arr, x);
+    printArray(arr, x);
+    return 0;
+}
